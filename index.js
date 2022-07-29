c568d6ef16e4c24c8833c753d0ff056b40f2129c
Lichi Liao <lichiliao617@gmail.com>
2022-07-29 15:08:31 +0800

    update with lodash, 1.2.2, fix load node error, make uniqle

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function NodeLoader(fileString) {
     const lodash = require('lodash')
 
     //function node
-    const functionNode = require("@node-red/nodes/core/function/10-function.js");
+    const functionNode = require("@node-red/nodes/core/function/10-function");
     const switchNode = require("@node-red/nodes/core/function/10-switch");
     const changeNode = require("@node-red/nodes/core/function/15-change");
     const rangeNode = require("@node-red/nodes/core/function/16-range");
@@ -20,7 +20,7 @@ module.exports = function NodeLoader(fileString) {
     const injectNode = require("@node-red/nodes/core/common/20-inject");
     const debugNode = require("@node-red/nodes/core/common/21-debug");
     const completeNode = require("@node-red/nodes/core/common/24-complete");
-    const catchNode = require("@node-red/nodes/core/common/25-catch.js");
+    // const catchNode = require("@node-red/nodes/core/common/25-catch.js");
     const statusNode = require("@node-red/nodes/core/common/25-status");
     const linkNode = require("@node-red/nodes/core/common/60-link");
     const commentNode = require("@node-red/nodes/core/common/90-comment");
@@ -65,7 +65,7 @@ module.exports = function NodeLoader(fileString) {
         "inject": injectNode,
         "debug": debugNode,
         "complete": completeNode,
-        "catch": catchNode,
+        // "catch": catchNode,
         "status": statusNode,
         "link in": linkNode,
         "link out": linkNode,
