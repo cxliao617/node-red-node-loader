8751e3121cbbd3e0eeb69e7495ca65f7870f394f
Lichi Liao <lichiliao617@gmail.com>
2022-07-29 15:19:29 +0800

    update with lodash, 1.2.3, fix load node error, make uniqle

--- index.js
+++ index.js
@@ -6,51 +6,51 @@ module.exports = function NodeLoader(fileString) {
     const lodash = require('lodash')
 
     //function node
-    const functionNode = require("@node-red/nodes/core/function/10-function");
-    const switchNode = require("@node-red/nodes/core/function/10-switch");
-    const changeNode = require("@node-red/nodes/core/function/15-change");
-    const rangeNode = require("@node-red/nodes/core/function/16-range");
-    const templateNode = require("@node-red/nodes/core/function/80-template");
-    const delayNode = require("@node-red/nodes/core/function/89-delay");
-    const triggerNode = require("@node-red/nodes/core/function/89-trigger");
-    const execNode = require("@node-red/nodes/core/function/90-exec");
-    const rbeNode = require("@node-red/nodes/core/function/rbe");
+    const functionNode = require("./nodes/core/function/10-function");
+    const switchNode = require("./nodes/core/function/10-switch");
+    const changeNode = require("./nodes/core/function/15-change");
+    const rangeNode = require("./nodes/core/function/16-range");
+    const templateNode = require("./nodes/core/function/80-template");
+    const delayNode = require("./nodes/core/function/89-delay");
+    const triggerNode = require("./nodes/core/function/89-trigger");
+    const execNode = require("./nodes/core/function/90-exec");
+    const rbeNode = require("./nodes/core/function/rbe");
 
     //common node
-    const injectNode = require("@node-red/nodes/core/common/20-inject");
-    const debugNode = require("@node-red/nodes/core/common/21-debug");
-    const completeNode = require("@node-red/nodes/core/common/24-complete");
-    // const catchNode = require("@node-red/nodes/core/common/25-catch.js");
-    const statusNode = require("@node-red/nodes/core/common/25-status");
-    const linkNode = require("@node-red/nodes/core/common/60-link");
-    const commentNode = require("@node-red/nodes/core/common/90-comment");
-    const unknownNode = require("@node-red/nodes/core/common/98-unknown");
+    const injectNode = require("./nodes/core/common/20-inject");
+    const debugNode = require("./nodes/core/common/21-debug");
+    const completeNode = require("./nodes/core/common/24-complete");
+    const catchNode = require("./nodes/core/common/25-catch.js");
+    const statusNode = require("./nodes/core/common/25-status");
+    const linkNode = require("./nodes/core/common/60-link");
+    const commentNode = require("./nodes/core/common/90-comment");
+    const unknownNode = require("./nodes/core/common/98-unknown");
 
     //network node
-    const tlsNode = require("@node-red/nodes/core/network/05-tls");
-    const httpproxyNode = require("@node-red/nodes/core/network/06-httpproxy");
-    const mqttNode = require("@node-red/nodes/core/network/10-mqtt");
-    const httpinNode = require("@node-red/nodes/core/network/21-httpin");
-    const httprequestNode = require("@node-red/nodes/core/network/21-httprequest");
-    const websocketNode = require("@node-red/nodes/core/network/22-websocket");
-    const tcpinNode = require("@node-red/nodes/core/network/31-tcpin");
-    const udpNode = require("@node-red/nodes/core/network/32-udp");
+    const tlsNode = require("./nodes/core/network/05-tls");
+    const httpproxyNode = require("./nodes/core/network/06-httpproxy");
+    const mqttNode = require("./nodes/core/network/10-mqtt");
+    const httpinNode = require("./nodes/core/network/21-httpin");
+    const httprequestNode = require("./nodes/core/network/21-httprequest");
+    const websocketNode = require("./nodes/core/network/22-websocket");
+    const tcpinNode = require("./nodes/core/network/31-tcpin");
+    const udpNode = require("./nodes/core/network/32-udp");
 
     //parser node
-    const csvNode = require("@node-red/nodes/core/parsers/70-CSV");
-    const htmlNode = require("@node-red/nodes/core/parsers/70-HTML");
-    const jsonNode = require("@node-red/nodes/core/parsers/70-JSON");
-    const xmlNode = require("@node-red/nodes/core/parsers/70-XML");
-    const yamlNode = require("@node-red/nodes/core/parsers/70-YAML");
+    const csvNode = require("./nodes/core/parsers/70-CSV");
+    const htmlNode = require("./nodes/core/parsers/70-HTML");
+    const jsonNode = require("./nodes/core/parsers/70-JSON");
+    const xmlNode = require("./nodes/core/parsers/70-XML");
+    const yamlNode = require("./nodes/core/parsers/70-YAML");
 
     //sequence node
-    const splitNode = require("@node-red/nodes/core/sequence/17-split");
-    const sortNode = require("@node-red/nodes/core/sequence/18-sort");
-    const batchNode = require("@node-red/nodes/core/sequence/19-batch");
+    const splitNode = require("./nodes/core/sequence/17-split");
+    const sortNode = require("./nodes/core/sequence/18-sort");
+    const batchNode = require("./nodes/core/sequence/19-batch");
 
     //storage node
-    const fileNode = require("@node-red/nodes/core/storage/10-file");
-    const watchNode = require("@node-red/nodes/core/storage/23-watch");
+    const fileNode = require("./nodes/core/storage/10-file");
+    const watchNode = require("./nodes/core/storage/23-watch");
 
     const nodeDict = {
         "function": functionNode,
@@ -65,7 +65,7 @@ module.exports = function NodeLoader(fileString) {
         "inject": injectNode,
         "debug": debugNode,
         "complete": completeNode,
-        // "catch": catchNode,
+        "catch": catchNode,
         "status": statusNode,
         "link in": linkNode,
         "link out": linkNode,
